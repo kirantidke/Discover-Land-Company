e0123b6a97bff9f22abc2ad3b56b2ea2eaa668da
kirantidke <kirantidke2013@gmail.com>
2021-08-05 22:52:28 +0530

    [Kiran]REFACTORED:added experinces page

--- js/header.js
+++ js/header.js
@@ -16,7 +16,7 @@ document.getElementById("header").innerHTML = `
                 <a href="../pages/experiences.html">Experiences</a>
             </li>
             <li class="nav-link">
-                <a href="../pages/gallery.html">Gallery</a>
+                <a href="../pages/gallary.html">Gallery</a>
             </li>
             <li class="nav-link">
                 <a href="../pages/press.html">Press</a>
