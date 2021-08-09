78345b1e5a7a089a08c6b48383d75908b7a4e974
kirantidke <kirantidke2013@gmail.com>
2021-08-09 22:56:17 +0530

    ADDED:header for foundation page

--- js/header.js
+++ js/header.js
@@ -16,7 +16,7 @@ document.getElementById("header").innerHTML = `
                 <a href="../pages/experinces.html">Experiences</a>
             </li>
             <li class="nav-link">
-                <a href="../pages/galary.html">Gallery</a>
+                <a href="../pages/gallery.html">Gallery</a>
             </li>
             <li class="nav-link">
                 <a href="../pages/press.html">Press</a>
